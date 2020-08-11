3e87b18fd0e76ec3de2396e98746b0c8ce220b52
Hernan Inga Arteaga <hm_inga@hotmail.com>
2020-08-11 11:11:16 -0500

    login redireccion

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {UsuarioService} from '../../services/usuario.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,7 @@ export class LoginComponent implements OnInit {
     mail:'',
     password:''
   }
-  constructor(private usuarioservice:UsuarioService) { }
+  constructor(private usuarioservice:UsuarioService,private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -21,9 +22,15 @@ export class LoginComponent implements OnInit {
     console.log(this.user)
     this.usuarioservice.login(this.user).subscribe(
       (res:any)=>{
-        if(res.length){localStorage.setItem('usuario',JSON.stringify(res[0]))} else {console.log('nada')} 
+        if(res.length){
+          localStorage.setItem('usuario',JSON.stringify(res[0]))
+          this.router.navigate(['/listar'])
+        } else {
+          localStorage.setItem('usuario',JSON.stringify('none'))
+          console.log('nada')
+        } 
         console.log(res)
-
+        
       }
     )
   }
